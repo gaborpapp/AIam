4f0229eca7c9c816a9fd207f12acf51f1f24eaf1
Alex Berman <alex@nimations.com>
2017-09-20 08:43:28 +0200

    Standardize PN test receiver
    After this change, the PN test receiver's visual appearance and UI behaviour is comparable to OSC output viewer.

--- tracking/pn/test_receiver.py
+++ tracking/pn/test_receiver.py
@@ -1,56 +1,220 @@
 #!/usr/bin/env python
 
+import math
 import argparse
 import threading
 from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
+from PyQt4 import QtCore, QtGui, QtOpenGL
 
 from receiver import PnReceiver, SERVER_PORT_BVH
 
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__))+"/../../movement_ai")
+from bvh import bvh_reader as bvh_reader_module
+from ui.floor_checkerboard import FloorCheckerboard
+from ui.window import Window
 
-import window
-from bvh.bvh_reader import BvhReader
+FLOOR_ARGS = {"num_cells": 26, "size": 26,
+              "board_color1": (.2, .2, .2, 1),
+              "board_color2": (.3, .3, .3, 1),
+              "floor_color": None,
+              "background_color": (0.0, 0.0, 0.0, 0.0)}
+CAMERA_Y_SPEED = .01
+CAMERA_KEY_SPEED = .1
+CAMERA_DRAG_SPEED = .1
+FRAME_RATE = 50
 
 parser = argparse.ArgumentParser()
-window.Window.add_parser_arguments(parser)
+Window.add_parser_arguments(parser)
 parser.add_argument("bvh")
+parser.add_argument("--camera", help="posX,posY,posZ,orientY,orientX",
+                    default="-3.767,-1.400,-3.485,-55.500,18.500")
 parser.add_argument("--host", default="localhost")
 parser.add_argument("--port", type=int, default=SERVER_PORT_BVH)
 parser.add_argument("-speed", type=float, default=1.0)
-parser.add_argument("-zoom", type=float, default=1.0)
-parser.add_argument("-unit-cube", action="store_true")
 parser.add_argument("-loop", action="store_true")
-parser.add_argument("-vertex-size", type=float, default=0)
 parser.add_argument("--z-up", action="store_true")
+parser.add_argument("--enable-floor", action="store_true")
 args = parser.parse_args()
 
-class BvhViewer(window.Window):
-    def __init__(self, *args):        
-        global bvh_reader
-        window.Window.__init__(self, *args)
-        self.reader = bvh_reader
+class MainWindow(Window):
+    def __init__(self, bvh_reader, args):
+        Window.__init__(self, args)
+        self._layout = QtGui.QVBoxLayout()
+        self._layout.setSpacing(0)
+        self._layout.setMargin(0)
+        self._layout.setContentsMargins(0, 0, 0, 0)
+        self._scene = Scene(bvh_reader, args)
+        self._layout.addWidget(self._scene)
+        self._create_menu()
+        self.setLayout(self._layout)
+        
+        if args.fullscreen:
+            self.give_keyboard_focus_to_fullscreen_window()
+            self._fullscreen_action.toggle()
 
-    def InitGL(self):
-        window.Window.InitGL(self)
-        glutMouseFunc(self._mouse_clicked)
-        glutMotionFunc(self._mouse_moved)
-        glEnable(GL_POINT_SMOOTH)
+    def _create_menu(self):
+        self._menu_bar = QtGui.QMenuBar()
+        self._layout.setMenuBar(self._menu_bar)
+        self._create_main_menu()
+        self._create_view_menu()
+
+    def _create_main_menu(self):
+        self._main_menu = self._menu_bar.addMenu("&Main")
+        self._add_show_camera_settings_action()
+        self._add_quit_action()
+        
+    def _add_show_camera_settings_action(self):
+        action = QtGui.QAction('Show camera settings', self)
+        action.triggered.connect(self._scene.print_camera_settings)
+        self._main_menu.addAction(action)
+        
+    def _add_quit_action(self):
+        action = QtGui.QAction("&Quit", self)
+        action.triggered.connect(QtGui.QApplication.exit)
+        self._main_menu.addAction(action)
+
+    def _create_view_menu(self):
+        self._view_menu = self._menu_bar.addMenu("View")
+        self._add_fullscreen_action()
+
+    def _add_fullscreen_action(self):
+        self._fullscreen_action = QtGui.QAction('Fullscreen', self)
+        self._fullscreen_action.setCheckable(True)
+        self._fullscreen_action.setShortcut('Ctrl+Return')
+        self._fullscreen_action.toggled.connect(self._toggled_fullscreen)
+        self._view_menu.addAction(self._fullscreen_action)
+
+    def _toggled_fullscreen(self):
+        if self._fullscreen_action.isChecked():
+            self.enter_fullscreen()
+        else:
+            self.leave_fullscreen()
+            
+    def keyPressEvent(self, event):
+        self._scene.keyPressEvent(event)
+        QtGui.QWidget.keyPressEvent(self, event)
+        
+class Scene(QtOpenGL.QGLWidget):
+    def __init__(self, bvh_reader, args):
+        self.bvh_reader = bvh_reader
+        self.args = args
+        self.margin = 0
+        self._set_camera_from_arg(args.camera)
+        self._dragging_orientation = False
+        self._dragging_y_position = False
+        QtOpenGL.QGLWidget.__init__(self)
+        self.setMouseTracking(True)
+        if args.enable_floor:
+            self._floor = FloorCheckerboard(**FLOOR_ARGS)
+
+        timer = QtCore.QTimer(self)
+        timer.setInterval(1000. / FRAME_RATE)
+        QtCore.QObject.connect(timer, QtCore.SIGNAL('timeout()'), self.updateGL)
+        timer.start()
+        
+    def _set_camera_from_arg(self, arg):
+        pos_x, pos_y, pos_z, orient_y, orient_z = map(float, arg.split(","))
+        self._set_camera_position([pos_x, pos_y, pos_z])
+        self._set_camera_orientation(orient_y, orient_z)
+
+    def _set_camera_position(self, position):
+        self._camera_position = position
+
+    def _set_camera_orientation(self, y_orientation, x_orientation):
+        self._camera_y_orientation = y_orientation
+        self._camera_x_orientation = x_orientation
+
+    def keyPressEvent(self, event):
+        r = math.radians(self._camera_y_orientation)
+        new_position = self._camera_position
+        key = event.key()
+        if key == QtCore.Qt.Key_A:
+            new_position[0] += CAMERA_KEY_SPEED * math.cos(r)
+            new_position[2] += CAMERA_KEY_SPEED * math.sin(r)
+            self._set_camera_position(new_position)
+            return
+        elif key == QtCore.Qt.Key_D:
+            new_position[0] -= CAMERA_KEY_SPEED * math.cos(r)
+            new_position[2] -= CAMERA_KEY_SPEED * math.sin(r)
+            self._set_camera_position(new_position)
+            return
+        elif key == QtCore.Qt.Key_W:
+            new_position[0] += CAMERA_KEY_SPEED * math.cos(r + math.pi/2)
+            new_position[2] += CAMERA_KEY_SPEED * math.sin(r + math.pi/2)
+            self._set_camera_position(new_position)
+            return
+        elif key == QtCore.Qt.Key_S:
+            new_position[0] -= CAMERA_KEY_SPEED * math.cos(r + math.pi/2)
+            new_position[2] -= CAMERA_KEY_SPEED * math.sin(r + math.pi/2)
+            self._set_camera_position(new_position)
+            return
+        
+    def sizeHint(self):
+        return QtCore.QSize(800, 600)
+
+    def initializeGL(self):
+        glClearColor(0.0, 0.0, 0.0, 0.0)
+        glClearAccum(0.0, 0.0, 0.0, 0.0)
+        glClearDepth(1.0)
+        glShadeModel(GL_SMOOTH)
         glEnable(GL_LINE_SMOOTH)
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-        self._y_orientation = 0.0
-        self._x_orientation = 0.0
+        glutInit(sys.argv)
+
+    def resizeGL(self, window_width, window_height):
+        self.window_width = window_width
+        self.window_height = window_height
+        if window_height == 0:
+            window_height = 1
+        glViewport(0, 0, window_width, window_height)
+        self.margin = 0
+        self.width = window_width - 2*self.margin
+        self.height = window_height - 2*self.margin
+        self._aspect_ratio = float(window_width) / window_height
+        self.min_dimension = min(self.width, self.height)
+
+    def paintGL(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        glTranslatef(self.margin, self.margin, 0)
+        self.render()
+
+    def configure_3d_projection(self, pixdx=0, pixdy=0):
+        self.fovy = 45
+        self.near = 0.1
+        self.far = 100.0
+
+        fov2 = ((self.fovy*math.pi) / 180.0) / 2.0
+        top = self.near * math.tan(fov2)
+        bottom = -top
+        right = top * self._aspect_ratio
+        left = -right
+        xwsize = right - left
+        ywsize = top - bottom
+        dx = -(pixdx*xwsize/self.width)
+        dy = -(pixdy*ywsize/self.height)
+
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glFrustum (left + dx, right + dx, bottom + dy, top + dy, self.near, self.far)
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+
+        glRotatef(self._camera_x_orientation, 1.0, 0.0, 0.0)
+        glRotatef(self._camera_y_orientation, 0.0, 1.0, 0.0)
+        glTranslatef(*self._camera_position)
 
     def render(self):
         self.configure_3d_projection(-100, 0)
-        glRotatef(self._x_orientation, 1.0, 0.0, 0.0)
-        glRotatef(self._y_orientation, 0.0, 1.0, 0.0)
-        if args.unit_cube:
-            self._draw_unit_cube()
+        camera_x = self._camera_position[0]
+        camera_z = self._camera_position[2]
+        if args.enable_floor:
+            self._floor.render(0, 0, camera_x, camera_z)
         self._draw_skeleton()
 
     def _draw_skeleton(self):
@@ -59,72 +223,63 @@ class BvhViewer(window.Window):
             return
         bvh_reader.set_pose_from_frame(pose, frame)
         vertices = pose.get_vertices()
-        edges = self.reader.vertices_to_edges(vertices)
-        if args.vertex_size > 0:
-            self._draw_vertices(vertices)
-        self._draw_edges(edges)
-
-    def _draw_vertices(self, vertices):
-        glPointSize(args.vertex_size)
-        glColor3f(0,0,0)
-        glBegin(GL_POINTS)
-        for vertex in vertices:
-            self._bvh_vertex(self._zoom_vertex(vertex))
-        glEnd()
-
-    def _draw_edges(self, edges):
-        glLineWidth(2.0)
-        glColor3f(0,0,0)
+        edges = self.bvh_reader.vertices_to_edges(vertices)
+        glColor3f(1, 1, 1)
+        glLineWidth(5.0)
         for edge in edges:
-            self._draw_line(self._zoom_vertex(edge.v1),
-                            self._zoom_vertex(edge.v2))
+            v1 = self.bvh_reader.normalize_vector_without_translation(edge.v1)
+            v2 = self.bvh_reader.normalize_vector_without_translation(edge.v2)
+            self._render_edge(v1, v2)
 
-    def _zoom_vertex(self, vertex):
-        return args.zoom * self.reader.normalize_vector(vertex)
-
-    def _draw_line(self, v1, v2):
+    def _render_edge(self, v1, v2):
         glBegin(GL_LINES)
-        self._bvh_vertex(v1)
-        self._bvh_vertex(v2)
+        self._vertex(v1)
+        self._vertex(v2)
         glEnd()
 
-    def _bvh_vertex(self, v):
-        glVertex3f(
-            v[bvh_coordinate_left],
-            v[bvh_coordinate_up],
-            v[bvh_coordinate_far])
+    def _vertex(self, worldpos):
+        if self.args.z_up:
+            glVertex3f(worldpos[0], worldpos[2], worldpos[1])
+        else:
+            glVertex3f(worldpos[0], worldpos[1], worldpos[2])
 
-    def _draw_unit_cube(self):
-        glColor4f(0,0,0,0.2)
-        glutWireCube(2.0)
+    def mousePressEvent(self, event):
+        if event.button() == QtCore.Qt.LeftButton:
+            self._dragging_orientation = True
+        elif event.button() == QtCore.Qt.RightButton:
+            self._dragging_y_position = True
 
-    def _mouse_clicked(self, button, state, x, y):
-        if button == GLUT_LEFT_BUTTON:
-            self._dragging_orientation = (state == GLUT_DOWN)
-        if state == GLUT_DOWN:
-            self._drag_x_previous = x
-            self._drag_y_previous = y
+    def mouseReleaseEvent(self, event):
+        self._dragging_orientation = False
+        self._dragging_y_position = False
+        self._drag_x_previous = event.x()
+        self._drag_y_previous = event.y()
 
-    def _mouse_moved(self, x, y):
+    def mouseMoveEvent(self, event):
+        x = event.x()
+        y = event.y()
         if self._dragging_orientation:
-            self._y_orientation += x - self._drag_x_previous
-            self._x_orientation -= y - self._drag_y_previous
+            self._set_camera_orientation(
+                self._camera_y_orientation + CAMERA_DRAG_SPEED * (x - self._drag_x_previous),
+                self._camera_x_orientation + CAMERA_DRAG_SPEED * (y - self._drag_y_previous))
+        elif self._dragging_y_position:
+            self._camera_position[1] += CAMERA_Y_SPEED * (y - self._drag_y_previous)
+        self._drag_x_previous = x
+        self._drag_y_previous = y
+
+    def print_camera_settings(self):
+        print "%.3f,%.3f,%.3f,%.3f,%.3f" % (
+            self._camera_position[0],
+            self._camera_position[1],
+            self._camera_position[2],
+            self._camera_y_orientation, self._camera_x_orientation)
 
 def receive():
     global frame
     for frame in pn_receiver.get_frames():
         pass
 
-if args.z_up:
-    bvh_coordinate_left = 0
-    bvh_coordinate_up = 2
-    bvh_coordinate_far = 1
-else:
-    bvh_coordinate_left = 0
-    bvh_coordinate_up = 1
-    bvh_coordinate_far = 2
-
-bvh_reader = BvhReader(args.bvh)
+bvh_reader = bvh_reader_module.BvhReader(args.bvh)
 bvh_reader.read()
 pose = bvh_reader.get_hierarchy().create_pose()
 frame = None
@@ -138,4 +293,7 @@ receiver_thread = threading.Thread(target=receive)
 receiver_thread.daemon = True
 receiver_thread.start()
 
-window.run(BvhViewer, args)
+app = QtGui.QApplication(sys.argv)
+window = MainWindow(bvh_reader, args)
+window.show()
+app.exec_()
